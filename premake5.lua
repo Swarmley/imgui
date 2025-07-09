e2c05c206a0096c967a1aba3ba2595a58e5dd37d
Michal Hvezda <Hvezda.mich@gmail.com>
2025-07-09 14:06:24 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,39 +1,41 @@
-project "ImGui"
-	kind "StaticLib"
-	language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
+project "imgui"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++20"
+   targetdir ("%{wks.location}" .. "/bin/%{cfg.buildcfg}" .. "/%{prj.name}")
+   architecture "x86_64"
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
+   includedirs {"", "%{include_directories.SDL}"}
+   libdirs{
+      "%{lib_directories.SDL_debug}"
+   }
+   files {
+   "backends/imgui_impl_sdl3.cpp",
+   "backends/imgui_impl_sdl3.h",
+   "backends/imgui_impl_dx12.h",
+   "backends/imgui_impl_dx12.cpp",
+   "backends/imgui_impl_win32.h",
+   "backends/imgui_impl_win32.cpp",
+   "imgui.h",
+   "imgui.cpp",
+   "imgui.h",
+   "imgui_demo.cpp",
+   "imgui_draw.cpp",
+   "imgui_internal.h",
+   "imgui_tables.cpp",
+   "imgui_widgets.cpp",
+   "imstb_rectpack.h",
+   "imstb_textedit.h",
+   "imstb_truetype.h",
+   "imconfig.h",
+}
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+   filter "configurations:Debug"
+      defines { "_DEBUG", }
+      symbols "On"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+   filter "configurations:Release"
+      optimize "On"
